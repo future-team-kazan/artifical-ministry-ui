112891be294d60afc1616f7dac21233ad43369f0
j.savicheva <j.savicheva@innopolis.ru>
2019-06-16 20:27:06 +0300

    dashboard

---
+++ src/app/pages/dashboard/dashboardInspectionResults/dashboardInspectionResults.directive.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.dashboard')
+    .directive('dashboardInspectionResults', dashboardInspectionResults);
+
+  /** @ngInject */
+  function dashboardInspectionResults() {
+    return {
+      restrict: 'E',
+      controller: 'DashboardInspectionResultsCtrl',
+      templateUrl: 'app/pages/dashboard/dashboardInspectionResults/dashboardInspectionResults.html'
+    };
+  }
+})();
