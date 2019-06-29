83db44affdb542c198ccd8c249ea77104144b111
j.savicheva <j.savicheva@innopolis.ru>
2019-06-29 16:42:51 +0300

    страница главная уведдмлений

--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -7,8 +7,9 @@
 
   angular.module('BlurAdmin.pages', [
     'ui.router',
+    'BlurAdmin.pages.mainPage',
 
-    'BlurAdmin.pages.dashboard',
+    // 'BlurAdmin.pages.dashboard',
     'BlurAdmin.pages.calendar',
     'BlurAdmin.pages.documents',
     'BlurAdmin.pages.edicts',
@@ -19,41 +20,41 @@
     'BlurAdmin.pages.statistics',
     'BlurAdmin.pages.finances',
     'BlurAdmin.pages.verifications',
-    'BlurAdmin.pages.notifications'
+    'BlurAdmin.pages.notifications',
     // 'BlurAdmin.pages.ui',
     // 'BlurAdmin.pages.components',
     // 'BlurAdmin.pages.form',
     // 'BlurAdmin.pages.tables',
     // 'BlurAdmin.pages.charts',
     // 'BlurAdmin.pages.maps',
-    // 'BlurAdmin.pages.profile',
+    'BlurAdmin.pages.profile'
   ])
       .config(routeConfig);
 
   /** @ngInject */
   function routeConfig($urlRouterProvider, baSidebarServiceProvider) {
-    $urlRouterProvider.otherwise('/dashboard');
+    $urlRouterProvider.otherwise('/main');
 
-    // baSidebarServiceProvider.addStaticItem({
-    //   title: 'Pages',
-    //   icon: 'ion-document',
-    //   subMenu: [{
-    //     title: 'Sign In',
-    //     fixedHref: 'auth.html',
-    //     blank: true
-    //   }, {
-    //     title: 'Sign Up',
-    //     fixedHref: 'reg.html',
-    //     blank: true
-    //   }, {
-    //     title: 'User Profile',
-    //     stateRef: 'profile'
-    //   }, {
-    //     title: '404 Page',
-    //     fixedHref: '404.html',
-    //     blank: true
-    //   }]
-    // });
+    baSidebarServiceProvider.addStaticItem({
+      title: 'Pages',
+      icon: 'ion-document',
+      subMenu: [{
+        title: 'Sign In',
+        fixedHref: 'auth.html',
+        blank: true
+      }, {
+        title: 'Sign Up',
+        fixedHref: 'reg.html',
+        blank: true
+      }, {
+        title: 'User Profile',
+        stateRef: 'profile'
+      }, {
+        title: '404 Page',
+        fixedHref: '404.html',
+        blank: true
+      }]
+    });
 
 
     // baSidebarServiceProvider.addStaticItem({
